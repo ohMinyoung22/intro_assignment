98973c3275399092ce67c173972fba802867368d
onMinyoung22 <minyoung_stat@dgu.ac.kr>
2022-04-26 17:25:15 +0900

    added mainpage.html

--- intro_assignment/main/views.py
+++ intro_assignment/main/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def showmain(request):
     return render(request, 'main/show.html')
+
+def showpage(request):
+    return render(request, 'main/mainpage.html')
