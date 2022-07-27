e746d4ca0d5bd02d70b2db3614b4cd391b9cc339
ohminyoung <minyoung_stat@dgu.ac.kr>
2022-07-28 01:29:46 +0900

    dw

--- intro_assignment/users/urls.py
+++ intro_assignment/users/urls.py
@@ -4,4 +4,5 @@ from .views import *
 app_name = "users"
 urlpatterns = [
     path('mypage/<int:user_id>', mypage, name="mypage"),
+    path('follow/<int:user_id>', follow, name="follow")
 ]
--- intro_assignment/users/views.py
+++ intro_assignment/users/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from main.models import Post
 from django.contrib.auth.models import User
 
@@ -6,4 +6,15 @@ from django.contrib.auth.models import User
 def mypage(request, user_id):
     user = get_object_or_404(User, pk = user_id)
     posts = Post.objects.filter(writer=user)
-    return render(request, 'users/mypage.html', {'posts' : posts})
+    return render(request, 'users/mypage.html', {'posts' : posts, 'user': user})
+
+def follow(request, user_id):
+    user = request.user
+    followed_user = get_object_or_404(User, pk=user_id)
+    is_follower = user.profile in followed_user.profile.followers.all()
+
+    if is_follower:
+        user.profile.followings.remove(followed_user.profile)
+    else:
+        user.profile.followings.add(followed_user.profile)
+    return redirect('user:mypage', followed_user.id)
